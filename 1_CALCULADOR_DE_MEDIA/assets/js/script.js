062772ddbcf45f20ddf4f6f1ada7afc0a305c551
Pedro <pedrohv20fernandes@gmail.com>
2022-03-08 13:26:56 -0300

    Atualizando o primeiro projeto

--- 1_CALCULADOR_DE_MEDIA/assets/js/script.js
+++ 1_CALCULADOR_DE_MEDIA/assets/js/script.js
@@ -1,24 +1,100 @@
-
 // Variaveis, Stings, console.log, função toFixed, operações metematicas, interpolação(concatenação), debug com console.log
 
-// Calcular media
-function calcularMedia(){
+// Não deixa passar numeros e caracteres estranhos
 
+function validaCampos() {
+  document.getElementById("seunome").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(chr) < 0)
+      return false;
+  };
+  document.getElementById("nota1").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("1234567890".indexOf(chr) < 0) return false;
+  };
+  document.getElementById("nota2").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("1234567890".indexOf(chr) < 0) return false;
+  };
+  document.getElementById("nota3").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("1234567890".indexOf(chr) < 0) return false;
+  };
+  document.getElementById("nota4").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("1234567890".indexOf(chr) < 0) return false;
+  };
+
+  document.getElementById("graus").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("-1234567890".indexOf(chr) < 0) return false;
+  };
+
+  document.getElementById("fireraid").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("-1234567890".indexOf(chr) < 0) return false;
+  };
+}
+
+validaCampos();
+
+// Calcular media
+function calcularMedia() {
+  let nota1 = document.getElementById("nota1").value;
+  let nota2 = document.getElementById("nota2").value;
+  let nota3 = document.getElementById("nota3").value;
+  let nota4 = document.getElementById("nota4").value;
+
+  let nome = document.getElementById("seunome").value;
+
+  let resultado = document.getElementById("resultado");
+
+  if (nota1 == "" || nota2 == "" || nota3 == "" || nota4 == "" || nome == "") {
+    resultado.innerHTML = "Preencha todos os campos!!!";
+    resultado.style.color = "red";
+  } else {
+    let resultadoFinal =
+      (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4)) / 4;
+    if (resultadoFinal >= 10) {
+      resultado.innerHTML = `Parabens ${nome} você conseguiu passar com a media de ${resultadoFinal.toFixed(
+        2
+      )} :D'`;
+      resultado.style.color = "green";
+    } else {
+      resultado.innerHTML = `${nome} infelizmente você não conseguiu passar com a media de ${resultadoFinal.toFixed(
+        2
+      )} D:'`;
+      resultado.style.color = "red";
+    }
+  }
 }
 
 // Conversores: fahrenheit para graus celsius e moedas
 
-function conversorGrau(){
+function conversorGrau() {
+  let campoCelsius = document.getElementById("graus").value;
 
+  let campoFireraid = document.getElementById("fireraid").value;
+
+  let calculoFahrenheit = (9 * parseFloat(campoCelsius)) / 5 + 32;
+
+  let resultadoFireraid = document.getElementById("resultadoFireraid");
+  resultadoFireraid.innerHTML = `Graus: ${campoCelsius} para fahrenheit é: ${calculoFahrenheit}`;
+
+  let calculoCelsius = (5 * parseFloat(campoFireraid) - 32) / 9;
+
+  let resultadoGraus = document.getElementById("resultadoGraus");
+  resultadoGraus.innerHTML = `fahrenheit: ${campoFireraid} para Graus é: ${calculoCelsius}`;
+
+  if (campoCelsius === "" && campoFireraid === "") {
+    let resultadoGeral = document.getElementById("resultadoGeral");
+    resultadoGeral.innerHTML = `TODOS OS CAMPOS ESTÃO VAZIOS!!!`;
+    resultadoGeral.style.display = "block";
+    resultadoGeral.style.color = "red";
+  } else {
+    resultadoGeral.style.display = "none";
+  }
 }
-
-function conversorMoeda(){
-    
-}
-
-
-
-
 
 // var nome = "Gui"
 // var nota1 = 9
