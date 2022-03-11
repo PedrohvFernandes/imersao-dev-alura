2e5e99af92df18f4dda80d2d3af5708e853ae14a
Pedro <pedrohv20fernandes@gmail.com>
2022-03-11 19:02:56 -0300

    Layout, input de video, delete de video, responsiv

--- 4_ALURAFLIX/assets/js/script.js
+++ 4_ALURAFLIX/assets/js/script.js
@@ -41,12 +41,74 @@ let listaFilmes = [
   },
 ];
 
-listaFilmes.map((item) => {
-  document.write(`
-  
-  <div class='div-filmes' id="div-filmes">
+// Adiciona mais um filme para lista de filmes
 
-    <div class='div-imagem-filme'>
+function adicionarFilme() {
+  let dadosNome = document.getElementById("nomedofilme").value;
+  let dadosNota = document.getElementById("notafilme").value;
+  let dadosSobre = document.getElementById("sobrefilme").value;
+  let dadosData = document.getElementById("datalancamento").value;
+  let urlImg = document.getElementById("linkimagem").value;
+  let urlTrailer = document.getElementById("linktrailer").value;
+  let erro = document.getElementById("erro");
+  let sucesso = document.getElementById("sucesso");
+
+  // 1ºVerifica se esta vazio o campo
+  // 2º Verificar se inseriu o link da imagem corretamente
+  // 3º Verificar se existe o filme
+  if (
+    verificaCampos(
+      dadosNome,
+      dadosNota,
+      dadosSobre,
+      dadosData,
+      urlImg,
+      urlTrailer
+    )
+  ) {
+    if (!jaExiste(dadosNome, urlImg)) {
+      listaFilmes.push({
+        descricao: {
+          Nome: dadosNome,
+          Nota: dadosNota,
+          Sobre: dadosSobre,
+          Data: dadosData,
+        },
+        linkimagem: urlImg,
+        linkvideo: urlTrailer,
+      });
+
+      exibir();
+      limpaCampos();
+      sucesso.innerHTML =
+        "Ebaaaa mais um filme pra lista :D veja o trailer caso você tenha colocado";
+      sucesso.style.color = "green";
+      erro.style.display = "none";
+      sucesso.style.display = "block";
+    } else {
+      erro.innerHTML =
+        "Todos os campos foram peenchidos corretamente, mas esse filme ja esta cadastrado, capa ou nome ja esta cadastrado";
+      erro.style.color = "red";
+      erro.style.display = "block";
+      sucesso.style.display = "none";
+    }
+  } else {
+    erro.innerHTML =
+      "Todos os campos tem que ser preenchidos corretamente. Já o trailer ele é opcional, mas lembre-se tem que ser do youtube";
+    erro.style.color = "red";
+    erro.style.display = "block";
+    sucesso.style.display = "none";
+  }
+}
+
+// Exibe o filme que ja estão no codigo na tela que foi adicionado na lista
+function exibir() {
+  let divConteudo = document.getElementById("div-filmes");
+  divConteudo.innerHTML = "";
+  listaFilmes.map((item) => {
+    divConteudo.innerHTML += `
+    
+  <div class='div-imagem-filme'>
       <button
       onclick="toggleVideo('hide')"
       role="button"
@@ -56,36 +118,180 @@ listaFilmes.map((item) => {
       <i class="fa-solid fa-xmark"></i>
       FECHAR O TRAILER
     </button>
-      <button onclick="toggleVideo()" role="button" class="botao btn">Ver o trailer</button>
+    <div class='div-button-img'>
+      <div class='div-button'>
+        <button onclick="toggleVideo()" role="button" class="botao btn">Ver o trailer</button>
+        <button onclick="removerFilme(event)" role="button" class="botao btn">Remover filme</button>
+      </div>
       <img class='filmes-imgs' src=${item.linkimagem}>
     </div>
 
-    <div class='descricao-filme-container'>
-
-      <div class='descricao-filme'> 
-        <p><span>Nome do filme:</span> ${item.descricao.Nome}</p>
-        <p><span>Nota: </span> ${item.descricao.Nota}</p>
-        <p><span>Sobre: </span>${item.descricao.Sobre}</p>
-        <p><span>Data de lançamento: </span>${item.descricao.Data}</p>
-      </div>
-
-    </div>
-    <div id="iframe" class="iframe-div">
-
  
-      <iframe
-      class="frame-youtube"
-      src="${item.linkvideo}"
-      frameborder="0"
-      allowfullscreen
-      >
-      </iframe>
-
-
+  <div class='descricao-filme-container'>
+    <div class='descricao-filme'>
+      <p><span>Nome do filme:</span>${item.descricao.Nome}</p>
+      <p><span>Nota: </span>${item.descricao.Nota}% gostaram desse filme</p>
+      <p><span>Sobre: </span>${item.descricao.Sobre}</p>
+      <p><span>Data de lançamento: </span>${item.descricao.Data}</p>
     </div>
+
   </div>
-  `);
-});
+
+  <div id="iframe" class="iframe-div">
+
+
+    <iframe
+    class="frame-youtube"
+    src="${item.linkvideo}"
+    frameborder="0"
+    allowfullscreen
+    >
+    </iframe>
+
+
+  </div>
+
+  </div>
+    
+    `;
+  });
+}
+exibir();
+
+function limpaCampos() {
+  document.getElementById("nomedofilme").value = "";
+  document.getElementById("notafilme").value = "";
+  document.getElementById("sobrefilme").value = "";
+  document.getElementById("datalancamento").value = "";
+  document.getElementById("linkimagem").value = "";
+  document.getElementById("linktrailer").value = "";
+  document.getElementById("erro").innerHTML = ``;
+  document.getElementById("sucesso").innerHTML = ``;
+  document.getElementById("link-da-imagem-errado").innerHTML = ``;
+}
+
+// verificar pra não deixcar pasasr caracter estranho em certos inputs
+function validaCampos() {
+  document.getElementById("nomedofilme").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if (
+      "1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(
+        chr
+      ) < 0
+    )
+      return false;
+  };
+
+  document.getElementById("notafilme").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if ("1234567890".indexOf(chr) < 0) return false;
+  };
+
+  document.getElementById("sobrefilme").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if (
+      "1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(
+        chr
+      ) < 0
+    )
+      return false;
+  };
+
+  document.getElementById("sobrefilme").onkeypress = function (e) {
+    let chr = String.fromCharCode(e.which);
+    if (
+      "/1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(
+        chr
+      ) < 0
+    )
+      return false;
+  };
+}
+validaCampos();
+
+// Verificar o tipo do arquivo da imagem e se os campos estão vazios
+function verificaCampos(
+  dadosNome,
+  dadosNota,
+  dadosSobre,
+  dadosData,
+  urlImg,
+  urlTrailer
+) {
+  if (
+    (dadosNome.trim() &&
+      dadosNota.trim() &&
+      dadosSobre.trim() &&
+      dadosData.trim() &&
+      urlImg.trim()) ||
+    urlTrailer.trim()
+  ) {
+    if (verificarDados(urlImg)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+// Verifica se é a imagem jpg, jpeg, png
+function verificarDados(urlImg) {
+  let linkDaImagemErrado = document.getElementById("link-da-imagem-errado");
+  if (
+    urlImg.endsWith(".jpg") ||
+    urlImg.endsWith(".jpeg") ||
+    urlImg.endsWith(".png")
+  ) {
+    linkDaImagemErrado.style.display = "none";
+    return true;
+  } else {
+    linkDaImagemErrado.innerHTML = `Você tem que inserir links que terminam com .PNG, JPG ou JPEG`;
+    linkDaImagemErrado.style.display = "block";
+    linkDaImagemErrado.style.color = "red";
+    return false;
+  }
+}
+
+// Verifica se o filme ja existe: nome e link da imagem
+// https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/
+
+// Todos eles são: hof -> High order function, igual o find Uma função que recebe uma callback como parametro e é um metodo exclusivo de array
+
+// Some ->O método some() testa se pelo menos algum dos elementos de um array passa no teste implementado por uma função atribuída. O proprio some ja retorna true independentemente do === ou !==
+
+// find ->O método find() retorna o valor do primeiro elemento de um array que retornar true para uma função de teste fornecida. Se não houver alguma, retorna undefined.
+
+// every -> O método every() testa se todos os elementos do array passam por um teste implementado por uma função fornecida.
+
+// map -> o map retorna um novo array
+
+// forEach -> não retorna nada
+function jaExiste(dadosNome, img) {
+  return listaFilmes.some(function (e) {
+    return e.descricao.Nome === dadosNome || e.linkimagem === img;
+  });
+}
+
+// Remove o filme da lista
+function removerFilme(event) {
+  let divFilmes = document.querySelector(".descricao-filme");
+  // Pegando o filho do divFilmes que é o P depois o texto da posição 1 no caso o nome do filme pois o split pega o que esta vindo apos o :, e o [1], pega o conteudo da posição 1
+  let name = divFilmes.children[0].innerText.split(":")[1];
+  console.log(name);
+  for (let i = 0; i < listaFilmes.length; i++) {
+    // Obs tive que retirar o espaço entre Nome do filme:Yesterday pois o name pega o texto no html e no objeto o nome do filme não tem espaço, logo os dois tem que ficar sem espaço para serem iguais
+    if (listaFilmes[i].descricao.Nome === name) {
+      // Splica recorta(deleta) o filme de acordo com indice diferente do delete que deleta mas deixa um espaço vazio
+      // Splice
+      // Opera com 1 ou mais (indefinidos parâmetros). O primeiro parâmetro é o index de onde deve iniciar a remoção, o segundo a quantidade de valores removidos(se não informado, removerá todos os valores do index início até o final da array), e do terceiro em diante serão valores novos que entrarão no lugar dos valores removidos. https://pt.stackoverflow.com/questions/344404/diferen%C3%A7a-entre-splice-e-slice https://pt.stackoverflow.com/questions/77622/como-buscar-um-determinado-objeto-dentro-de-um-array
+      listaFilmes.splice(i, 1);
+    }
+  }
+  // Removemos os parentes nó arvore -> removendo a div que é o container de tudo
+  event.target.parentNode.parentNode.parentNode.remove();
+}
 
 // Trailer fo filme
 function toggleVideo(state) {
@@ -101,23 +307,6 @@ function toggleVideo(state) {
     "*"
   );
 }
-
-// Adiciona mais um filme para lista de filmes
-function adicionarFilme() {
-  let dados = document.getElementById("descricao");
-  let urlimg = document.getElementById("linkimagem");
-
-  dados.value = "";
-  urlimg.value = "";
-}
-
-function verficarDados() {
-  // verificar pra não deixcar pasasr caracter estranho
-  // Verificar o tipo do arquivo
-}
-verficarDados();
-
-function removerFilme() {}
 
 // let listaFilmes = ['Yesterday', 'A chegada', 'Escola de Rock']
 //                   // Elemento: 1 2 3
